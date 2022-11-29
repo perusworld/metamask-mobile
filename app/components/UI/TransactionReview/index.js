2044437d9feb2761570de5332b6ac810ada0d359
Tomás SANTOS <tommasini15@gmail.com>
2022-11-29 19:13:02 +0000

    update styles for 4 inches device and use TagUrl component

--- app/components/UI/TransactionReview/index.js
+++ app/components/UI/TransactionReview/index.js
@@ -58,10 +58,10 @@ const createStyles = (colors) =>
       ...fontStyles.bold,
     },
     actionViewWrapper: {
-      height: Device.isMediumDevice() ? 230 : 290,
+      height: Device.isMediumDevice() ? 170 : 290,
     },
     actionViewChildren: {
-      height: 200,
+      height: 220,
     },
     accountTransactionWrapper: {
       flex: 1,
@@ -441,7 +441,7 @@ class TransactionReview extends PureComponent {
               }
             >
               <View style={styles.actionViewChildren}>
-                <ScrollView>
+                <ScrollView nestedScrollEnabled>
                   <View
                     style={styles.accountTransactionWrapper}
                     onStartShouldSetResponder={() => true}
